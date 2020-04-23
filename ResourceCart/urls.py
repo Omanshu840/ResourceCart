218b6d44f3e0570ad077ac1cb8647f073e38b67b
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-04-24 00:59:56 +0530

    Laying the pipeline of project

--- ResourceCart/urls.py
+++ ResourceCart/urls.py
@@ -20,5 +20,11 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='ShopHome'),
+    path('about/', views.about, name='AboutUs'),
+    path('contact/', views.contact, name='ContactUs'),
+    path('tracker/', views.tracker, name='TrackingStatus'),
+    path('search/', views.search, name='Search'),
+    path('productview/', views.productview, name='ProductView'),
+    path('checkout/', views.checkout, name='Checkout'),
 ]
--- ResourceCart/views.py
+++ ResourceCart/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return HttpResponse("About Page")
+
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+
+def tracker(request):
+    return HttpResponse("Tracker Page")
+
+
+def search(request):
+    return HttpResponse("Search Page")
+
+
+def productview(request):
+    return HttpResponse("Product Page")
+
+
+def checkout(request):
+    return HttpResponse("Checkout Page")
