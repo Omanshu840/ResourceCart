3fbf1a29b57a1f43e8d012763bf0f730f227ab00
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-06 01:14:23 +0530

    Adding Cart and Product Page

--- shop/views.py
+++ shop/views.py
@@ -3,15 +3,26 @@ from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
+
 # Create your views here.
 
 
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n // 4 + ceil((n / 4) - (n // 4))
-    params = {'no_of_slides': nSlides, 'product': products, 'range': range(1, nSlides)}
+    # products = Product.objects.all()
+    # n = len(products)
+    # nSlides = n // 4 + ceil((n / 4) - (n // 4))
 
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for item in cats:
+        prod = Product.objects.filter(category=item)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # allProds = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
@@ -31,8 +42,11 @@ def search(request):
     return HttpResponse("Search Page")
 
 
-def productview(request):
-    return HttpResponse("Product Page")
+def productview(request, myid):
+
+    product = Product.objects.filter(id=myid)
+
+    return render(request, 'shop/product.html', {'product': product[0]})
 
 
 def checkout(request):
